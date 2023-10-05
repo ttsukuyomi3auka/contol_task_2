01f8813ed020c7ac6d1cdfc9ace96f31267e0bc7
tsukuyomi <tsumi@mail>
2023-10-05 11:38:18 +0500

    first commit

---
+++ bin/contol_task_2.dart
@@ -0,0 +1,5 @@
+import 'package:contol_task_2/contol_task_2.dart' as contol_task_2;
+
+void main(List<String> arguments) {
+  
+}
---
+++ test/contol_task_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:contol_task_2/contol_task_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
